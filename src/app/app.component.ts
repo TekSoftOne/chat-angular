65e2096063d044f79effb4de3664f45a7e2fd584
Joseph Nguyen <joseph@teksoft1.com>
2020-08-28 00:02:17 +0700

    menu bar - part 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,13 @@ import { NgForm } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'chat-angular';
   password: string;
   email: string;
-
+  openSideNav = false;
   public onSubmit(form: NgForm): void {
     if (form.valid) {
       console.log('submitted success!');
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,25 +2,30 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule} from '@angular/material/input';
+import { MatInputModule } from '@angular/material/input';
 import { MatCardModule } from '@angular/material/card';
-
-
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
 @NgModule({
   imports: [
     MatButtonModule,
     BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatToolbarModule,
   ],
   exports: [
     MatButtonModule,
     BrowserAnimationsModule,
     MatFormFieldModule,
     MatInputModule,
-    MatCardModule
-
+    MatCardModule,
+    MatSidenavModule,
+    MatToolbarModule,
   ],
   declarations: [],
   providers: [],
 })
-
-export class MaterialModule { }
+export class MaterialModule {}
