230ef9ee1d90ef5dc705faabe6147d6963f672c2
Tien Song Nguyen <tiennsloit@gmail.com>
2020-08-28 13:11:37 +0700

    (html-css-material) split the nav bar into header and list items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,4 @@ import { NgForm } from '@angular/forms';
 })
 export class AppComponent {
   title = 'chat-angular';
-  password: string;
-  email: string;
-  public onSubmit(form: NgForm): void {
-    if (form.valid) {
-      console.log('submitted success!');
-    } else {
-      return;
-    }
-  }
 }
