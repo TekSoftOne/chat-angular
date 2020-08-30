2f31ca55344eb80fccfb3fcb9f08214c37998a7d
Tien Song Nguyen <tiennsloit@gmail.com>
2020-08-30 20:00:28 +0700

    add save message using firestore

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFirestore } from 'angularfire2/firestore';
-import { Observable } from 'rxjs';
+import { Observable, Subscriber, of, throwError } from 'rxjs';
+import { NgForm } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import { catchError, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-dashboard',
@@ -9,7 +12,9 @@ import { Observable } from 'rxjs';
 })
 export class DashboardComponent implements OnInit {
   public messages: Observable<any>;
-  constructor(private db: AngularFirestore) {}
+  public message = '';
+  private readonly COLLECTION_NAME = 'messages';
+  constructor(private db: AngularFirestore, private httpClient: HttpClient) {}
 
   ngOnInit(): void {
     this.messages = this.db
@@ -27,5 +32,45 @@ export class DashboardComponent implements OnInit {
       // });
       // })
       ();
+  }
+
+  public sendMessage(): Observable<boolean> {
+    if (this.message.length > 0) {
+      this.saveMessage()
+        .pipe(
+          catchError((error) => {
+            console.log('send error');
+            return throwError(error);
+          }),
+          tap(() => (this.message = '')),
+          tap(() => console.log('send success!'))
+        )
+        .subscribe();
+    }
+
+    return of(false);
+  }
+
+  private saveMessage(): Observable<void> {
+    const moment = new Date();
+    return new Observable<void>((subscriber) => {
+      this.db
+        .collection(this.COLLECTION_NAME)
+        .doc('1')
+        .collection(moment.getFullYear().toString())
+        .doc((moment.getMonth() + 1).toString())
+        .collection(moment.getDate().toString())
+        .add({
+          content: this.message,
+          dateTime: moment,
+          senderId: '1',
+          read: false,
+        })
+        .then(() => {
+          subscriber.next();
+          subscriber.complete();
+        })
+        .catch((err) => subscriber.error(err));
+    });
   }
 }
